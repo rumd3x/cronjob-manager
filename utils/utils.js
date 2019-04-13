80bad9313eead331bf2dcaf138629c72475ade8e
rumd3x <edmurcardoso@gmail.com>
2019-04-13 19:02:39 -0300

    Added cron service management
    Signed-off-by: rumd3x <edmurcardoso@gmail.com>

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+var shell = require('shelljs');
 
 const getBiggest = (property, array) => {
     let biggest = null;
@@ -17,7 +18,7 @@ const getBiggest = (property, array) => {
 };
 
 const rewriteCronFile = (jobs) => {
-    const cronFilePath = './node-persist/jobs.crontab';
+    const cronFilePath = './.node-persist/jobs.crontab';
     if (fs.existsSync(cronFilePath)) {
         fs.unlinkSync(cronFilePath);
     }
@@ -26,10 +27,16 @@ const rewriteCronFile = (jobs) => {
         let cronEntry = makeCommand(job) + ` >> /var/log/${job.name}.log 2>&1 \n`;
         fs.appendFileSync(cronFilePath, cronEntry);
     });
+    restartCron();
 };
 
 const makeCommand = (job) => {
     return `${job.cron.trim()} ${job.commandType.trim()} ${job.command.trim()}`;
 };
+
+const restartCron = () => {
+    const serviceBinary = shell.which("service");
+    const result = shell.exec(`${serviceBinary} cron reload`);
+}
 
 module.exports = { getBiggest, rewriteCronFile }
